8f277f5200747c5e9685cfb1e8edde9a414793d3
menthe <adrien.le.mentheour@gmail.com>
2026-01-22 16:22:07 +0100

    🔧 modular sections

--- src/cms/queries/pages/basic.ts
+++ src/cms/queries/pages/basic.ts
@@ -1,6 +1,11 @@
+import sections from "../utils/sections";
+
 export default `
     id
     _modelApiKey    
     title
     slug
+    content {
+        ${sections}
+    }
 `;
---
+++ src/cms/queries/utils/sections.ts
@@ -0,0 +1,31 @@
+export default `
+    ... on BlockquoteModuleRecord {
+        _modelApiKey
+        id
+    }
+    ... on ImagesModuleRecord {
+        _modelApiKey
+        id
+    }
+    ... on IndentedTextModuleRecord {
+        _modelApiKey
+        id
+    }
+    ... on LinksModuleRecord {
+        _modelApiKey
+        id
+    }
+    ... on TitleContentModuleRecord {
+        _modelApiKey
+        id
+        title
+    }
+    ... on TwoColsModuleRecord {
+        _modelApiKey
+        id
+    }
+    ... on WysiwygModuleRecord {
+        _modelApiKey
+        id
+    }
+`;
