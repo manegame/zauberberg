8212c7e69a86f107f43cc10e62070360cffd7ef7
Clément Lemoine <clementlemoine.cl@gmail.com>
2026-01-29 18:23:09 +0100

    💄work page

--- src/cms/queries/pages/works.ts
+++ src/cms/queries/pages/works.ts
@@ -1,5 +1,31 @@
+import responsiveImg from "../utils/responsiveImg";
+
 export default `
     id
     _modelApiKey
     slug
+    videos {
+        id
+        video {
+            _modelApiKey
+            id
+            slug
+            name
+            client
+            preview {
+                id
+                wiredriveUrl
+                poster {
+                    url
+                    ${responsiveImg}
+                }
+            }
+            directors {
+                id
+                name
+                slug
+                _modelApiKey
+            }
+        }
+    }
 `;
