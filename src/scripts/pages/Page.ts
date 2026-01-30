9a18b27d60929e673bbf8e8623311c4366823d45
Clément Lemoine <clementlemoine.cl@gmail.com>
2026-01-30 15:05:24 +0100

    🔧💫 custom swap rework

--- src/scripts/pages/Page.ts
+++ src/scripts/pages/Page.ts
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 import Zauberberg from "../Zauberberg";
 
 export default class Page {
@@ -5,9 +6,12 @@ export default class Page {
     initialized: boolean;
     destroyed: boolean;
     app: Zauberberg;
+    template: string;
 
-    constructor() {
+    constructor(template: string, doc: Document = window.document) {
         this.app = new Zauberberg();
+        this.template = template;
+        this.container = doc.getElementById("page") as HTMLElement;
         this.initialized = false;
         this.destroyed = false;
     }
@@ -18,5 +22,33 @@ export default class Page {
 
     destroy() {
         this.destroyed = true;
+    }
+
+    prepareTransitionIn(from: Page) {
+        gsap.set(this.container, { opacity: 0 });
+    }
+
+    transitionIn(from: Page): Promise<any> | gsap.core.Timeline {
+        const tl = gsap.timeline({ paused: true });
+
+        tl.to(this.container, {
+            opacity: 1,
+            duration: 0.2,
+            ease: "power2.out",
+        });
+
+        return tl.play();
+    }
+
+    transitionOut(to: Page): Promise<any> | gsap.core.Timeline {
+        const tl = gsap.timeline({ paused: true });
+
+        tl.to(this.container, {
+            opacity: 0,
+            duration: 0,
+            ease: "power2.out",
+        });
+
+        return tl.play();
     }
 }
--- src/scripts/pages/index.ts
+++ src/scripts/pages/index.ts
@@ -1,13 +1,22 @@
 import DirectorsPage from "./Directors";
 import Director from "./Director";
-import type Page from "./Page";
+import Page from "./Page";
+
+const DEFAULT_PAGE = Page;
 
 const PAGE_BY_TEMPLATE: Record<string, typeof Page> = {
     directors_page: DirectorsPage,
     director: Director,
 };
 
-export default (template: string | undefined) => {
-    const PageClass = PAGE_BY_TEMPLATE[template || ""] || null;
+export const getPageClass = (template: string) => {
+    const PageClass = PAGE_BY_TEMPLATE[template || ""] || DEFAULT_PAGE;
     return PageClass;
+};
+
+export const getPage = (template: string, doc?: Document) => {
+    console.log(doc);
+
+    const PageClass = getPageClass(template);
+    return new PageClass(template, doc);
 };
