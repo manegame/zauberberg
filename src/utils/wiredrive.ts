4a06f471d21f3009a00b088abda1f870ff7f137c
Clément Lemoine <clementlemoine.cl@gmail.com>
2026-02-19 19:08:41 +0100

    🐛 various checks to prevent wrong/empty data from CMS

--- src/utils/wiredrive.ts
+++ src/utils/wiredrive.ts
@@ -10,21 +10,29 @@ export const getDataFromRSS = (rssUrl: string) => {
                 attributesGroupName: "",
             });
 
-            const xmlDoc = parser.parse(str);
+            try {
+                const xmlDoc = parser.parse(str);
 
-            const video = xmlDoc.rss.channel.item["media:content"];
-            const posters = xmlDoc.rss.channel.item["media:thumbnail"];
+                const video = xmlDoc.rss.channel.item["media:content"];
+                const posters = xmlDoc.rss.channel.item["media:thumbnail"];
 
-            // get poster where width is bigger
-            const poster = posters.reduce((prev: any, current: any) => {
-                return parseInt(current.width) > parseInt(prev.width)
-                    ? current
-                    : prev;
-            });
+                // get poster where width is bigger
+                const poster = posters.reduce((prev: any, current: any) => {
+                    return parseInt(current.width) > parseInt(prev.width)
+                        ? current
+                        : prev;
+                });
 
-            return {
-                video,
-                poster,
-            };
+                return {
+                    video,
+                    poster,
+                };
+            } catch (error) {
+                console.error("Failed to parse RSS feed:", rssUrl);
+                return {
+                    video: null,
+                    poster: null,
+                };
+            }
         });
 };
