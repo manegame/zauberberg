9b6fa95daee9d692351f84eee85ca2117f1a2cce
Clément Lemoine <clementlemoine.cl@gmail.com>
2026-03-05 15:12:05 +0100

    ZAUB-28 External directors

--- src/cms/queries/pages/video.ts
+++ src/cms/queries/pages/video.ts
@@ -19,6 +19,7 @@ export default `
             slug
         }
     }
+    externaldirectors
     videoRss
     previewRss
     awardIcon {
--- src/cms/queries/pages/works.ts
+++ src/cms/queries/pages/works.ts
@@ -22,6 +22,7 @@ export default `
                 slug
                 _modelApiKey
             }
+            externaldirectors
             awardIcon {
                 ${img}
             }
